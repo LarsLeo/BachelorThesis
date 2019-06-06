9047b71f0a4a041ee0eacfc435ed6480cf007db3
Lars Leo Grätz <lars.leo.graetz@gmail.com>
2019-06-06 16:57:41 +0200

    removed metadata

--- sality/out/gcc-release/src/entities/SalityConstants.d
+++
@@ -1,4 +0,0 @@
-../out/gcc-release/src/entities/SalityConstants.o: \
- entities/SalityConstants.cc entities/SalityConstants.h
-
-entities/SalityConstants.h:
--- sality/out/gcc-release/src/messages/url_pack_m.d
+++
@@ -1,6 +0,0 @@
-../out/gcc-release/src/messages/url_pack_m.o: messages/url_pack_m.cc \
- messages/url_pack_m.h /opt/omnetpp-5.4.1/include/omnetpp.h
-
-messages/url_pack_m.h:
-
-/opt/omnetpp-5.4.1/include/omnetpp.h:
